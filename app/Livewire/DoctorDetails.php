2c625e923761b1d9ea1c6118e2e8ef0bb75dc613
Liam <lkhant616@gmail.com>
2024-01-22 19:58:51 +0630

    some changes

--- app/Livewire/DoctorDetails.php
+++ app/Livewire/DoctorDetails.php
@@ -4,17 +4,52 @@ namespace App\Livewire;
 
 use App\Models\Doctor;
 use Livewire\Component;
+use App\Models\Schedule;
+use Livewire\Attributes\Validate;
 
 class DoctorDetails extends Component
 {
-    public $id;
-    // public function mount($id)
-    // {
-    //     $this->id = $id;
-    // }
-    public function render()
+    
+    public $doctor_id;
+    #[Validate('required')]
+
+    public $day;
+    #[Validate('required|min:3')]
+
+    public $to;
+    #[Validate('required|min:3')]
+
+    public $from;
+   public function mount($doctor_id)
     {
         
-        return view('livewire.doctor-details');
+        $this->doctor_id = $doctor_id;
+    }
+    public function store()
+    {
+        $this->validate();
+        $schedule = new Schedule();
+        $schedule->day = $this->day;
+        $schedule->to = $this->to;
+        $schedule->from = $this->from;
+        $schedule->doctor_id = $this->doctor_id;
+        $schedule->save();
+        session()->flash('status', 'Schedule successfully added.');
+        
+        return $this->redirect('/admin/doctor/details/'.$this->doctor_id,navigate:true);
+    }
+    
+    public function delete($id)
+    {
+        Schedule::where('id',$id)->delete();
+        session()->flash('status', 'Schedule successfully deleted.');
+        
+        return $this->redirect('/admin/doctor/details/'.$this->doctor_id,navigate:true);    }
+    
+    public function render()
+    {
+        $doctor =Doctor::where('id',$this->doctor_id)->first();
+        $schedules = Schedule::where('doctor_id',$this->doctor_id) ->get();
+        return view('livewire.doctor-details')->with(compact('doctor','schedules'));
     }
 }
